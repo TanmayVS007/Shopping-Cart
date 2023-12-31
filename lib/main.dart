ad5da9958ceea47a8e72bcecc1a88431f15cd767
SONAWANE TANMAY VIVEK <124717859+TanmayVS007@users.noreply.github.com>
2024-01-01 00:38:14 +0530

    Project-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_cart/core/store.dart';
+import 'package:shopping_cart/screens/home_screen.dart';
+import 'package:shopping_cart/screens/login_screen.dart';
+import 'package:shopping_cart/widgets/themes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'firebase_options.dart';
+import 'screens/cart_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(VxState(store: MyStore(), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,7 +22,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Demo"
+    themeMode: ThemeMode.system,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+      routes: {
+        HomeScreen.routeName: (context) => const HomeScreen(),
+        LoginScreen.routeName: (context) => const LoginScreen(),
+        CartScreen.routeName: (context) => const CartScreen(),
+      },
+      initialRoute: HomeScreen.routeName,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
